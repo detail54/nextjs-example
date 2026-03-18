44d3a7f992b2e1b5f835e5f4e5624c53fa3d0a56
daehyun <detail54@nate.com>
2026-03-18 14:37:36 +0900

    react-query-devtools 추가 - @tanstack/react-query-devtools 패키지 추가 - Providers 컴포넌트에 ReactQueryDevtools 통합

--- lib/Providers.tsx
+++ lib/Providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState } from 'react'
 import { createQueryClient } from './queryClient'
 
@@ -9,5 +10,11 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   // 렌더링마다 새 인스턴스 생성 방지
   const [queryClient] = useState(() => createQueryClient())
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {/* 개발 환경에서만 표시 */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
